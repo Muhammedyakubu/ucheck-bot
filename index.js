ad154cc33e74a9630a35d3cc8abd04cdc1c8b3ea
Muhammed Yakubu <72368510+Muhammedyakubu@users.noreply.github.com>
2021-12-24 17:44:37 -0500

    i feel like it's logically correct...?

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ username = "your_utor_id"
 
 password = "your_password"
 
-login_page = 'https://idpz.utorauth.utoronto.ca/idp/profile/SAML2/Redirect/SSO?execution=e1s1';
-ucheck_home = 'https://ucheck.utoronto.ca';
-ucheck_form = 'https://ucheck.utoronto.ca/questionnaire?org=22f9f13e-e281-4688-9d95-324d4ae4e43b&pt=6475dcb1-9421-4057-8cad-c1604fb173a4';
-login_button = "body > div.container > div > div.col-md-4 > div.login-box > form > button";
-start_button = "MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButton-fullWidth";
+const login_page = 'https://idpz.utorauth.utoronto.ca/idp/profile/SAML2/Redirect/SSO?execution=e1s1',
+ucheck_home = 'https://ucheck.utoronto.ca',
+ucheck_form = 'https://ucheck.utoronto.ca/questionnaire?org=22f9f13e-e281-4688-9d95-324d4ae4e43b&pt=6475dcb1-9421-4057-8cad-c1604fb173a4',
+login_button = "body > div.container > div > div.col-md-4 > div.login-box > form > button",
+start_button = "MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButton-fullWidth",
+form_input_class = "sc-pRFZy iIPyzR"
 
 
 
@@ -37,7 +38,13 @@ async function start_form() {
 function fill_ucheck_forms(){
     await (window.location.href == ucheck_form)
 
-    for 
+    let form_inputs = document.getElementsByClassName(form_input_class)
+
+
+    for (let i = 1; i < 15; i++) {
+        if (i == 1 || i % 2 == 0) 
+            form_inputs[i].click();
+    }
 }
 
 

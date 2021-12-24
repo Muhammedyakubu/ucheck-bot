17053cbeb37360f49bd39f3760f652c0f95db81e
Muhammed Yakubu <72368510+Muhammedyakubu@users.noreply.github.com>
2021-12-24 17:53:14 -0500

    okay now it's up to date

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 //==================FILL IN YOUR CREDENTIALS=====================//
-let username = "your_utor_id",
+let username = "your_utor_id"
 
-password = "your_password";
+password = "your_password"
 
 
 
@@ -11,7 +11,8 @@ ucheck_home = 'https://ucheck.utoronto.ca',
 ucheck_form = 'https://ucheck.utoronto.ca/questionnaire?org=22f9f13e-e281-4688-9d95-324d4ae4e43b&pt=6475dcb1-9421-4057-8cad-c1604fb173a4',
 login_button = "body > div.container > div > div.col-md-4 > div.login-box > form > button",
 start_button = "MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButton-fullWidth",
-form_input_class = "sc-pRFZy iIPyzR";
+form_input_class = "sc-pRFZy iIPyzR",
+submit_button = document.querySelector("#root > div > div > div > div.MuiContainer-root.sc-pRgDJ.sc-pZdvY.dlLhKU.MuiContainer-maxWidthMd > main > div > div > div > div > div > button > span.MuiButton-label");
 
 
 
@@ -47,4 +48,7 @@ function fill_ucheck_forms(){
         if (i == 1 || i % 2 == 0) 
             form_inputs[i].click();
     }
+
+    submit_button.click()
 }
+
